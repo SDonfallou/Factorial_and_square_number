f0043e3bf2bb26780af439c51cd0615ec8abd32f
SDonfallou <99264517+SDonfallou@users.noreply.github.com>
2022-05-03 21:39:40 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-     Scanner scn = new Scanner(System.in);
-     String parola_input;
-    System.out.println("Dammi la parola");
-     parola_input  = scn.next();
+
+     String parola_input = LettereInglese.parola_dato();
      LettereInglese.contlettere(parola_input);
      
   }
@@ -17,14 +15,23 @@ class LettereInglese {
      int cont=0;
     
      int lung = parola_input.length();
-    for (int i = 0 ; i < lung; i++) {    
-       if (parola_input.charAt(i) == 'J' || parola_input.charAt(i) == 'j') cont ++;
-       else if (parola_input.charAt(i) == 'K' || parola_input.charAt(i) == 'k') cont ++;
-            else  if (parola_input.charAt(i) == 'X' || parola_input.charAt(i) == 'x') cont ++;
-                  else  if (parola_input.charAt(i) == 'Y' || parola_input.charAt(i) == 'y') cont ++;
-                        else  if (parola_input.charAt(i) == 'W' || parola_input.charAt(i) == 'w') cont ++;
-		} 
+
+    char lettere_inglese [] = {'j','k','x','y','w'};
+      for (int i =0; i<lung; i++){
+        for (int j=0; j<lettere_inglese.length; j++){
+          if (parola_input.toLowerCase().charAt(i)==lettere_inglese[j] ) cont ++;
+        }
+      }
+      
      if (cont>0) System.out.println("Ci sono " + cont + " Carattere  inglese in questa parola");
       else System.out.println("Non ci sono Carattere  inglese in questa parola");
 	}
+
+  public static String parola_dato() {
+      Scanner scn = new Scanner(System.in);
+     String parola_interno;
+     System.out.println("Dammi la parola");
+     parola_interno  = scn.nextLine();
+    return parola_interno;
+  }
 }
